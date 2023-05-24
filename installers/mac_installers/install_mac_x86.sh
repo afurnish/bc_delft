570d8bb5459a8e15d68d97a3e144c9affcfd10b9
Aaron Furnish <73606176+afurnish@users.noreply.github.com>
2023-05-24 20:34:35 +0100

    Small update

--- installers/mac_installers/install_mac_x86.sh
+++ installers/mac_installers/install_mac_x86.sh
@@ -1,3 +1,3 @@
 curl -O https://repo.anaconda.com/miniconda/Miniconda3-latest-MacOSX-x86_64.sh
-yes | echo -ne '\n' | sleep 10| bash Miniconda3-latest-MacOSX-x86_64.sh
+yes | bash Miniconda3-latest-MacOSX-x86_64.sh
 
